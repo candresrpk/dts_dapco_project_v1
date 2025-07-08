d882c3d6777f75178a247a97a3cbb59ce311f103
Carlos Reyes <andrespk3000@gmail.com>
2025-07-07 21:10:40 -0500

    encuestas model first view

--- my_apps/dapco/models.py
+++ my_apps/dapco/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    pass
+
+
+class Encuesta(models.Model):
+    administrador = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)    
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'encuesta'
+        verbose_name_plural = 'encuestas'
+
+    def __str__(self):
+        return f'Encuesta: {self.nombre} - creada por {self.administrador}'
+    
+    
+
+class Distribucion(models.Model):
+    edad = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    barrio = models.CharField(max_length=3)
+    estrato = models.IntegerField()
+    
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'distribucion'
+        verbose_name_plural = 'distribuciones'
+    
+    def __str__(self):
+        return f'{self.edad} - {self.sexo} - {self.barrio} - {self.estrato}'
+    
+
+class Pregunta(models.Model):
+    
+    class TipoChoiches(models.TextChoices):
+        Abierta = 'Abierta'
+        Opciones = 'Opciones'
+    
+    pregunta = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100, choices=TipoChoiches.choices, default=TipoChoiches.Opciones)
+    encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'pregunta'
+        verbose_name_plural = 'preguntas'
+    
+    def __str__(self):
+        return f'{self.pregunta}'
+    
+
+class Opcion(models.Model):
+    opcion = models.CharField(max_length=100)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        verbose_name = 'opcion'
+        verbose_name_plural = 'opciones'
+    
+    def __str__(self):
+        return f'{self.opcion}'
+    
+
+class Respuesta(models.Model):
+    
+    encuestador = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)    
+    opcion = models.ForeignKey(Opcion, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'respuesta'
+        verbose_name_plural = 'respuestas'
+    
+    def __str__(self):
+        return f'{self.pregunta} respuesta {self.opcion}'
